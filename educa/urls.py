04eb9b79b5ef15511b071177d861bc7f44767523
vssj-codes <vssjcodes@gmail.com>
2025-11-09 20:15:49 +0530

    ch13: manage course modules and their content

--- educa/urls.py
+++ educa/urls.py
@@ -1,7 +1,8 @@
 """
 URL configuration for educa project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views. For more information
+please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
@@ -11,7 +12,8 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import the include() function: from django.urls import include,
+        path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
@@ -23,9 +25,14 @@ from django.urls import include, path
 
 urlpatterns = [
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
-    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path(
+        "accounts/logout/",
+        auth_views.LogoutView.as_view(),
+        name="logout",
+    ),
     path("admin/", admin.site.urls),
     path("course/", include("courses.urls")),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
