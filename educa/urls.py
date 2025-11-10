7a25bc6f424a8eec75983ae7465ebd65c45ebe9b
vssj-codes <vssjcodes@gmail.com>
2025-11-10 13:36:10 +0530

    ch14: courses catalog

--- educa/urls.py
+++ educa/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
+from courses.views import CourseListView
 
 urlpatterns = [
     path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
@@ -32,6 +33,7 @@ urlpatterns = [
     ),
     path("admin/", admin.site.urls),
     path("course/", include("courses.urls")),
+    path("", CourseListView.as_view(), name="course_list"),
 ]
 
 if settings.DEBUG:
