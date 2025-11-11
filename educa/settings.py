7c6ce3dd92cf790bd8474be3196000fd99977342
vssj-codes <vssjcodes@gmail.com>
2025-11-11 21:16:37 +0530

    ch15: building RESTful API

--- educa/settings.py
+++ educa/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "embed_video",
     "debug_toolbar",
     "redisboard",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -155,3 +156,9 @@ INTERNAL_IPS = [
 CACHE_MIDDLEWARE_ALIAS = "default"
 CACHE_MIDDLEWARE_SECONDS = 60 * 15  # 15 minutes
 CACHE_MIDDLEWARE_KEY_PREFIX = "educa"
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ]
+}
