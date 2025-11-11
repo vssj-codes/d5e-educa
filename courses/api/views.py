6e50b420eee3c4264725378b6f37b443e7b11d09
vssj-codes <vssjcodes@gmail.com>
2025-11-11 23:11:26 +0530

    ch15: creating viewsets and routers

--- courses/api/views.py
+++ courses/api/views.py
@@ -1,16 +1,29 @@
 from rest_framework import generics
-from courses.api.serializers import SubjectSerializer
-from courses.models import Subject
+from courses.api.serializers import CourseSerializer, SubjectSerializer
+from courses.models import Course, Subject
 from django.db.models import Count
 from courses.api.pagination import StandardPagination
+from rest_framework import viewsets
 
 
-class SubjectListView(generics.ListAPIView):
-    queryset = Subject.objects.annotate(total_courses=Count("courses"))
-    serializer_class = SubjectSerializer
+# class SubjectListView(generics.ListAPIView):
+#     queryset = Subject.objects.annotate(total_courses=Count("courses"))
+#     serializer_class = SubjectSerializer
+#     pagination_class = StandardPagination
+
+
+# class SubjectDetailView(generics.RetrieveAPIView):
+#     queryset = Subject.objects.annotate(total_courses=Count("courses"))
+#     serializer_class = SubjectSerializer
+
+
+class CourseViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Course.objects.prefetch_related("modules")
+    serializer_class = CourseSerializer
     pagination_class = StandardPagination
 
 
-class SubjectDetailView(generics.RetrieveAPIView):
+class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Subject.objects.annotate(total_courses=Count("courses"))
     serializer_class = SubjectSerializer
+    pagination_class = StandardPagination
